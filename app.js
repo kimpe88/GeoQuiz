a8ed7bf5f28a7dad0dd1e266d7ab03f24224696e
Kim Persson <kimpersson88@gmail.com>
2015-02-06 23:52:03 +0100

    Testing Codeship CI

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Custom routes
 app.use('/', index);
 app.use('/play', play);
 
