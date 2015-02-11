da08a18826e1f577d0bbe4bdee2c22e37d05938e
jmasko <jhtmasko@gmail.com>
2015-02-11 12:43:24 +0100

    Added icon to google maps

--- public/javascripts/geolocation_map.js
+++ public/javascripts/geolocation_map.js
@@ -1,55 +1,57 @@
 // Requests the user location and inserts a Google Maps view with the user location marked
 // on the map
-var geolocationMap = function(){
-  var map;
 
-  function initialize() {
-    var mapOptions = {
-      zoom: 12
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+var geolocationMap = function () {
+    var map;
 
-    // Try HTML5 geolocation
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = new google.maps.LatLng(position.coords.latitude,
-                                         position.coords.longitude);
+    function initialize() {
+        var mapOptions = {
+            zoom: 12
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-        var infowindow = new google.maps.InfoWindow({
-          map: map,
-          position: pos,
-          content: 'Location found using HTML5.'
-        });
+        // Try HTML5 geolocation
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-        map.setCenter(pos);
-      }, function() {
-        handleNoGeolocation(true);
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleNoGeolocation(false);
-    }
-  }
 
-  function handleNoGeolocation(errorFlag) {
-    var content;
-    if (errorFlag) {
-      content = 'Error: The Geolocation service failed.';
-    } else {
-      content = 'Error: Your browser doesn\'t support geolocation.';
+                var icon = "images/viking icon.png";
+
+                var marker = new google.maps.Marker({
+                    position: pos,
+                    map: map,
+                    icon: icon
+                });
+
+                map.setCenter(pos);
+            }, function () {
+                handleNoGeolocation(true);
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleNoGeolocation(false);
+        }
     }
 
-    var options = {
-      map: map,
-      position: new google.maps.LatLng(60, 105),
-      content: content
-    };
+    function handleNoGeolocation(errorFlag) {
+        var content;
+        if (errorFlag) {
+            content = 'Error: The Geolocation service failed.';
+        } else {
+            content = 'Error: Your browser doesn\'t support geolocation.';
+        }
 
-    var infowindow = new google.maps.InfoWindow(options);
-    map.setCenter(options.position);
-  }
+        var options = {
+            map: map,
+            position: new google.maps.LatLng(60, 105),
+            content: content
+        };
 
-  google.maps.event.addDomListener(window, 'load', initialize);
+        var infowindow = new google.maps.InfoWindow(options);
+        map.setCenter(options.position);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 };
 geolocationMap();
