067e2ecae3465439dd864385e230c31aa2b2c758
Kim Persson <kimpersson88@gmail.com>
2015-05-26 22:02:03 +0200

    Fixed problems with failing tests from last commit

--- tests/tile_tests.js
+++ tests/tile_tests.js
@@ -3,39 +3,44 @@ var utils = require('./utils');
 var Tile = require('../models/tile');
 
 describe('testing tiles', function () {
-    var tile;
-    beforeEach(function (done) {
-        tile = new Tile({
-            x: 50,
-            y: 50,
-            owner: "test",
-            score: 10
-        });
-        tile.save(done);
+  var tile;
+  beforeEach(function (done) {
+    tile = new Tile({
+      owner: "test",
+      score: 10,
+      geo: [59.19, 18.03]
     });
-    afterEach(function (done) {
-        tile.remove(done);
-    });
+    tile.save(done);
+  });
+  afterEach(function (done) {
+    tile.remove(done);
+  });
 
-    it('get score of tile by coordinates', function (done) {
-        Tile.find({x: 50, y: 50}, function (err, results) {
-            expect(results[0].score).toEqual(10);
-            return done();
-        });
+  it('get score of tile by coordinates', function (done) {
+    Tile.find([59.19, 18.03], function (err, results) {
+      if(err)
+        throw err;
+      expect(results[0].score).toEqual(10);
+      return done();
     });
+  });
 
-    it('counts tiles owned', function (done) {
-        Tile.find({owner: "test"}, function (err, results) {
-            expect(results.length).toEqual(1);
-            return done();
-        });
+  it('counts tiles owned', function (done) {
+    Tile.find({owner: "test"}, function (err, results) {
+      if(err)
+        throw err;
+      expect(results.length).toEqual(1);
+      return done();
     });
+  });
 
-    it('claim test tile (50,50)', function (done) {
-        Tile.find({x: 50, y: 50}, function (err, results) {
-            results[0].owner = "doomviking";
-            expect(results[0].owner).toEqual("doomviking");
-            return done();
-        });
+  it('claim test tile (50,50)', function (done) {
+    Tile.find([59.19, 18.03], function (err, results) {
+      if(err)
+        throw err;
+      results[0].owner = "doomviking";
+      expect(results[0].owner).toEqual("doomviking");
+      return done();
     });
+  });
 });
